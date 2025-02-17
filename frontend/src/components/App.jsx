7b305c47cb5e7f1cdce5c937168938096f6f4b25
juanzeen <jziignerdev@gmail.com>
2025-02-17 17:10:54 -0300

    fix: adjusts in navbar and setting doubts in home

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,24 @@
 import "../assets/css/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 import Home from "../pages/Home";
 import ProcessoSeletivo from "../pages/ProcessoSeletivo";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route element={<Home />} path="/SiteSoulCodeJr" />
-          <Route element={<ProcessoSeletivo />} path="/SiteSoulCodeJr/processo-seletivo" />
+          <Route
+            element={<ProcessoSeletivo />}
+            path="/SiteSoulCodeJr/processo-seletivo"
+          />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import {Route} from "react-router-dom"
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./assets/css/index.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Duvidas from "./components/Duvidas";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar />
     <App />
-    <Duvidas />
-    <Footer />
   </React.StrictMode>
 );
