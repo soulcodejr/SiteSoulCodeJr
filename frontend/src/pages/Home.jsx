41cfdb8781a1a06f6f7ec3d88cdb6fbf524253dd
juanzeen <jziignerdev@gmail.com>
2025-02-26 19:16:35 -0300

    feat: reorganizing sections in a folder and switching sections-container to app.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import Duvidas from "../components/Duvidas";
-import Plans from "../components/Plans";
+import Duvidas from "../sections/Duvidas";
+import Plans from "../sections/Plans";
+import Contrate from "../sections/Contrate"
 import './Home.css'
 
 
 export default function Home() {
   return (
     <div className="home">
-
       <Plans />
-
       <Duvidas />
       <Contrate/>
     </div>
--- frontend/src/sections/Contrate.jsx
+++ frontend/src/sections/Contrate.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { MdOutlineEmail, MdOutlinePhone, MdOutlineLocationOn } from "react-icons/md";
+import {
+  MdOutlineEmail,
+  MdOutlinePhone,
+  MdOutlineLocationOn,
+} from "react-icons/md";
 
-import Form from '../components/Form'
+import Form from "../components/Form";
 
 import "./Contrate.css";
 
 const Contrate = () => {
   return (
-    <div className="contrate-container outfit-normal">
+    <div className="outer">
+      <button className="section-button">Contatos</button>
+      <div className="contrate-container outfit-normal">
       <div className="text-container">
         <h2>Tem interesse em nos contratar?</h2>
         <p>
@@ -16,13 +22,25 @@ const Contrate = () => {
         </p>
 
         <div className="infos-container">
-          <span> <MdOutlineEmail/> Soulcodejr@gmail.com</span>
-          <span> <MdOutlinePhone/> (22) xxxxx-xxxx</span>
-          <span> <MdOutlineLocationOn/> Av. Alberto Lamego, 2000 - Parque Califórnia, Campos dos Goytacazes - RJ</span>
+          <span>
+            {" "}
+            <MdOutlineEmail /> Soulcodejr@gmail.com
+          </span>
+          <span>
+            {" "}
+            <MdOutlinePhone /> (22) xxxxx-xxxx
+          </span>
+          <span>
+            {" "}
+            <MdOutlineLocationOn /> Av. Alberto Lamego, 2000 - Parque
+            Califórnia, Campos dos Goytacazes - RJ
+          </span>
         </div>
       </div>
-      <Form handleSubmit={()=> console.log()}/>
+      <Form handleSubmit={() => console.log()} />
     </div>
+    </div>
+
   );
 };
 
