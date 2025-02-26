e45601f029f992b1b19be7b90864267595a39a80
juanzeen <jziignerdev@gmail.com>
2025-02-26 17:33:21 -0300

    fix: conflicts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,15 @@
-import Plans from "../components/Plans";
-import Duvidas from "../sections/Duvidas";
-import Contrate from '../sections/Contrate'
 import Duvidas from "../components/Duvidas";
-import Plans from "../components/Plans";
-import './Home.css'
+import Plans from "../components/Plans"
+//import Contrate from "../sections/Contrate";
 
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
-
       <Plans />
-
       <Duvidas />
-      <div>
 
-      </div>
     </div>
   );
 }
