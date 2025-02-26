f9e011cb036dfb9ca4d4777a672a58b0d7628df0
juanzeen <jziignerdev@gmail.com>
2025-02-26 17:27:21 -0300

    fix: conflicts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import Plans from "../components/Plans";
+import Duvidas from "../sections/Duvidas";
+import Contrate from '../sections/Contrate'
 import Duvidas from "../components/Duvidas";
 import Plans from "../components/Plans";
 import './Home.css'
@@ -6,7 +9,7 @@ import './Home.css'
 export default function Home() {
   return (
     <div className="home">
-     
+
       <Plans />
 
       <Duvidas />
@@ -16,4 +19,3 @@ export default function Home() {
     </div>
   );
 }
-
