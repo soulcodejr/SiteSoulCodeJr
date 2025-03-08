d8f48609f7ca0caca77a69c3fda3141b89ac5d1f
Gabriel Gravina <gabrielgravina01@gmail.com>
2025-03-08 12:46:09 -0300

    fixing stack icons based on plan name

--- frontend/src/components/PlansCard.jsx
+++ frontend/src/components/PlansCard.jsx
@@ -2,15 +2,25 @@ import './PlansCard.css'
 import { PiStackSimple } from "react-icons/pi";
 import { FaCheck } from "react-icons/fa6";
 import { MdArrowOutward } from "react-icons/md";
+import React, { Component } from 'react';
 
-
+import { StackSimple, Stack, StackPlus } from '@phosphor-icons/react';
 const PlansCard = ( { plan }) => {
+
+    const getStackIcon = () => {
+        const icons = {
+            Basic: StackSimple,
+            Professional: Stack,
+            Enterprises: StackPlus
+        }
+        return icons[plan.name] || StackSimple
+    }
 
     return (
         <div className="plans-card__div outfit-normal">
             <div className="plans-card__header">
                 <div className="plan-card__title">
-                    <PiStackSimple size={15} className='plan-card__icon--basic'/>
+                    {React.createElement(getStackIcon(), {size: 22})}
                     <p>{plan.name}</p>
                 </div>
 
@@ -38,7 +48,7 @@ const PlansCard = ( { plan }) => {
                     </div>
 
                 ))}
-                </div>
+            </div>
         </div>
     );
 }
