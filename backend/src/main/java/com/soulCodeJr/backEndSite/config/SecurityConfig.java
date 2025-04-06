c8f471f1aac8cc91fab09fbfba2f065a69081176
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-04-05 21:04:58 -0300

    feat: cria a autenticação com basic authentication, cria a Role, o admin e adiciona uma future no envio de emails.

--- backend/src/main/java/com/soulCodeJr/backEndSite/config/SecurityConfig.java
+++ backend/src/main/java/com/soulCodeJr/backEndSite/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
    @Autowired
    private AdminService adminService;
 
